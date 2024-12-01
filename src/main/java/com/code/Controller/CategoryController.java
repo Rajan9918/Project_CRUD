2acfa397d9bc2604a8dca42dff93f0bd3ad2bf2f
Rajan <rajangard7@gmail.com>
2024-12-01 23:04:39 +0530

    second commit

--- src/main/java/com/code/Controller/CategoryController.java
+++ src/main/java/com/code/Controller/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+//category controller
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
